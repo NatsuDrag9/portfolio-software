5acfc6e30bf4fa2ec61e8e53ae7aae64ec7a6f85
NatsuDrag9 <rohitimandi9@zoho.com>
2023-12-20 22:38:04 +0100

    Added styles for contact icons and media query

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -4,24 +4,24 @@ import AnimatedLetters from '../AnimatedLetters'
 import { useEffect, useRef, useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { MapContainer, Marker, TileLayer, Popup } from 'react-leaflet'
-import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faGithub, faLinkedin, faMedium } from '@fortawesome/free-brands-svg-icons'
 
 function Contact() {
   const [letterClass, setLetterClass] = useState('text-animate')
   const email = 'rohitimandi9@zoho.com'
-  const markerRef = useRef();
-  const timeoutRef = useRef();
+  const markerRef = useRef()
+  const timeoutRef = useRef()
 
   useEffect(() => {
     timeoutRef.current = setTimeout(() => {
       setLetterClass('text-animate-hover')
-    }, 4000);
+    }, 4000)
 
     return () => {
       // Clear the timeout when the component unmounts
-      clearTimeout(timeoutRef.current);
-    };
-  }, [timeoutRef.current]);
+      clearTimeout(timeoutRef.current)
+    }
+  }, [timeoutRef.current])
 
   const handleMouseOver = () => {
     if (markerRef.current) {
@@ -49,6 +49,8 @@ function Contact() {
           <div className="body">
             <p>
               Feel free to send me a mail at {email} or connect with me on{''}
+            </p>
+            <div className="icons">
               <a
                 target="_blank"
                 rel="noreferrer"
@@ -56,7 +58,21 @@ function Contact() {
               >
                 <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
               </a>
-            </p>
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://github.com/NatsuDrag9"
+              >
+                <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+              </a>
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://rohitimandi.medium.com/"
+              >
+                <FontAwesomeIcon icon={faMedium} color="#4d4d4e" />
+              </a>
+            </div>
           </div>
         </div>
         <div className="map-wrap">
