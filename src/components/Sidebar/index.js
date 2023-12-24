1e2aa433beb95ab29d83fdd910e60a9bfa9f03a9
NatsuDrag9 <rohitimandi9@zoho.com>
2023-12-24 13:12:14 +0100

    Added vertical alignment for mobile devices

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -3,46 +3,60 @@ import { Link, NavLink } from 'react-router-dom'
 import LogoD1 from '../../assets/images/logo-d1.svg'
 // import LogoSubtitle from '../../assets/images/logo_sub_2.svg'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope, faHome, faUser, faBriefcase } from '@fortawesome/free-solid-svg-icons'
-import {faGithub, faLinkedin, faMedium} from '@fortawesome/free-brands-svg-icons';
+import {
+  faEnvelope,
+  faHome,
+  faUser,
+  faBriefcase,
+} from '@fortawesome/free-solid-svg-icons'
+// import {faGithub, faLinkedin, faMedium} from '@fortawesome/free-brands-svg-icons';
+import { useState } from 'react'
 
 function Sidebar() {
+  const [menuOpen, setMenuOpen] = useState(false)
+
   return (
-    <div className="nav-bar">
-      <Link className="logo" to="/">
-        <img src={LogoD1} alt="logo" className="logo" />
-        {/* <img className="sub-logo" src={LogoSubtitle} alt="logo-subtitle" /> */}
-      </Link>
-      <nav>
-        <NavLink exact="true" activeclassname="active" to="/">
-          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
-        </NavLink>
-        <NavLink
-          exact="true"
-          activeclassname="active"
-          className="work-link"
-          to="/work"
-        >
-          <FontAwesomeIcon icon={faBriefcase} color="#4d4d4e" />
-         </NavLink> 
-        <NavLink
-          exact="true"
-          activeclassname="active"
-          className="about-link"
-          to="/about"
-        >
-          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
-        </NavLink>
-        <NavLink
-          exact="true"
-          activeclassname="active"
-          className="contact-link"
-          to="/contact"
-        >
-          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
-        </NavLink>
-      </nav>
-      {/* <ul>
+    <>
+      <div className="navbar-menu" onClick={() => setMenuOpen(!menuOpen)}>
+        <span className={menuOpen ? 'clockwise' : ''}></span>
+        <span className={menuOpen ? 'hide' : ''}></span>
+        <span className={menuOpen ? 'anticlockwise' : ''}></span>
+      </div>
+      <div className={menuOpen ? "nav-bar open" : "nav-bar close"}>
+        <Link className="logo" to="/">
+          <img src={LogoD1} alt="logo" className="logo" />
+          {/* <img className="sub-logo" src={LogoSubtitle} alt="logo-subtitle" /> */}
+        </Link>
+        <nav>
+          <NavLink exact="true" activeclassname="active" to="/">
+            <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+          </NavLink>
+          <NavLink
+            exact="true"
+            activeclassname="active"
+            className="work-link"
+            to="/work"
+          >
+            <FontAwesomeIcon icon={faBriefcase} color="#4d4d4e" />
+          </NavLink>
+          <NavLink
+            exact="true"
+            activeclassname="active"
+            className="about-link"
+            to="/about"
+          >
+            <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+          </NavLink>
+          <NavLink
+            exact="true"
+            activeclassname="active"
+            className="contact-link"
+            to="/contact"
+          >
+            <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+          </NavLink>
+        </nav>
+        {/* <ul>
         <li>
             <a target="_blank" rel='noreferrer' href='https://www.linkedin.com/in/rohit-imandi/'>
                 <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
@@ -59,7 +73,8 @@ function Sidebar() {
             </a>
         </li>
       </ul> */}
-    </div>
+      </div>
+    </>
   )
 }
 
